c66516b913d48c7e259275f6bfde8e255ca4f8f4
MitchellFBL01 <gokudera_1995@yahoo.es>
2023-12-20 21:36:50 -0400

    Calculadora

---
+++ Calculadora.py
@@ -0,0 +1,89 @@
+import tkinter as tk
+from tkinter import ttk
+from math import sqrt
+
+def calculo(operacion):
+    try:
+        resultado = str(format(eval(operacion),".2f"))
+    except:
+        resultado = "ERROR"
+    pantalla.delete(0, tk.END)
+    pantalla.insert(tk.END, resultado)
+
+def rais(rais):
+    b = format(sqrt(int(pantalla.get())),".5f") 
+    pantalla.delete(0, tk.END)
+    pantalla.insert(0, b)
+
+def back_space():
+    b = pantalla.get()
+    pantalla.delete(0, tk.END)
+    pantalla.insert(0, b[:-1])
+
+calculadora = tk.Tk()
+
+style = ttk.Style()
+style.configure('pantalla.TEntry', bg='#000000')
+
+#--------------------------------- Pantalla ---------------------------
+
+pantalla = ttk.Entry(calculadora, width = 14, style= 'pantalla.TEntry', justify= "right")
+pantalla.grid(column = 1, row = 0, columnspan= 4, padx= 10, pady= 10)
+
+#--------------------------------- Teclas ---------------------------
+#--------------------------------- Fila 1 ---------------------------
+button7 = ttk.Button(calculadora, text= "7", width= 3, command=lambda:pantalla.insert(tk.END, "7"))
+button8 = ttk.Button(calculadora, text= "8", width= 3, command=lambda:pantalla.insert(tk.END, "8"))
+button9 = ttk.Button(calculadora, text= "9", width= 3, command=lambda:pantalla.insert(tk.END, "9"))
+buttonDiv = ttk.Button(calculadora, text= u"\u00F7", width= 3, command=lambda:pantalla.insert(tk.END, "/"))
+
+button7.grid(column = 1, row= 2)
+button8.grid(column = 2, row= 2)
+button9.grid(column = 3, row= 2)
+buttonDiv.grid(column = 4, row= 2)
+
+#--------------------------------- Fila 2 ---------------------------
+button4 = ttk.Button(calculadora, text= "4", width= 3, command=lambda:pantalla.insert(tk.END, "4"))
+button5 = ttk.Button(calculadora, text= "5", width= 3, command=lambda:pantalla.insert(tk.END, "5"))
+button6 = ttk.Button(calculadora, text= "6", width= 3, command=lambda:pantalla.insert(tk.END, "6"))
+buttonMul = ttk.Button(calculadora, text= "*", width= 3, command=lambda:pantalla.insert(tk.END, "*"))
+
+button4.grid(column = 1, row= 3)
+button5.grid(column = 2, row= 3)
+button6.grid(column = 3, row= 3)
+buttonMul.grid(column = 4, row= 3)
+
+#--------------------------------- Fila 3 ---------------------------
+button1 = ttk.Button(calculadora, text= "1", width= 3, command=lambda:pantalla.insert(tk.END, "1"))
+button2 = ttk.Button(calculadora, text= "2", width= 3, command=lambda:pantalla.insert(tk.END, "2"))
+button3 = ttk.Button(calculadora, text= "3", width= 3, command=lambda:pantalla.insert(tk.END, "3"))
+buttonRes = ttk.Button(calculadora, text= "-", width= 3, command=lambda:pantalla.insert(tk.END, "-"))
+
+button1.grid(column = 1, row= 4)
+button2.grid(column = 2, row= 4)
+button3.grid(column = 3, row= 4)
+buttonRes.grid(column = 4, row= 4)
+
+#--------------------------------- Fila 4 ---------------------------
+button0 = ttk.Button(calculadora, text= "0", width= 3, command=lambda:pantalla.insert(tk.END, "0"))
+buttonComa = ttk.Button(calculadora, text= ",", width= 3, command=lambda:pantalla.insert(tk.END, "."))
+buttonIgual = ttk.Button(calculadora, text= "=", width= 3, command=lambda:calculo(pantalla.get()))
+buttonSum = ttk.Button(calculadora, text= "+", width= 3, command=lambda:pantalla.insert(tk.END, "+"))
+
+button0.grid(column = 1, row= 5)
+buttonComa.grid(column = 2, row= 5)
+buttonIgual.grid(column = 3, row= 5)
+buttonSum.grid(column = 4, row= 5)
+
+#--------------------------------- Fila 5 ---------------------------
+buttonC = ttk.Button(calculadora, text= "DEL", width= 3, command= back_space)
+buttonDel = ttk.Button(calculadora, text= "C", width= 3, command=lambda:pantalla.delete(0, tk.END))
+buttonRais = ttk.Button(calculadora, text= "√", width= 3, command=lambda:rais(pantalla.get()))
+buttonEx = ttk.Button(calculadora, text= "^", width= 3, command=lambda:pantalla.insert(tk.END, "**"))
+
+buttonC.grid(column = 1, row= 1)
+buttonDel.grid(column = 2, row= 1)
+buttonRais.grid(column = 3, row= 1)
+buttonEx.grid(column = 4, row= 1)
+
+calculadora.mainloop()
